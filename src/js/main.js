5c5be001a91872cc06946de500c3cdc1ce0e7225
Stepan_Yefimov <Stepan_Yefimov@epam.com>
2017-09-06 12:17:30 +0300

    Think workshop is ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
-require('../css/normalize.css')
-require('../css/main.less')
-console.log('main.js')
+require('../css/normalize.css');
+require('../css/main.less');
+const x = 'main.js';
+console.log(x)
